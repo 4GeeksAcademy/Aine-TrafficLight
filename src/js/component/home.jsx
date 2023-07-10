178e2d5c4a4f41155c6d84df0521d31c382352cd
Josseth Sanchez <128737955+Aine20@users.noreply.github.com>
2023-07-10 22:26:46 +0000

    Making a traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [clickedButton, setClickedButton] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setClickedButton(index === clickedButton ? null : index);
+  };
+
+  return (
+    <div className="container">
+      <div className="structure">
+        <button
+          className={`circle Red ${clickedButton === 0 ? "highlighted" : ""}`}
+          onClick={() => handleButtonClick(0)}
+        ></button>
+        <button
+          className={`circle Yellow ${
+            clickedButton === 1 ? "highlighted" : ""
+          }`}
+          onClick={() => handleButtonClick(1)}
+        ></button>
+        <button
+          className={`circle Green ${clickedButton === 2 ? "highlighted" : ""}`}
+          onClick={() => handleButtonClick(2)}
+        ></button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
